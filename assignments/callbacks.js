0abdba3492814b669fc96490dcb42b72d9525308
Jacqueline <jacquelineomollo@gmail.com>
2019-05-24 20:06:24 -0400

    most of callback problems done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -24,22 +24,33 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+getLength(items, length => {
+  console.log(items);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
+console.log(items.pop());
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return x + y;
 }
+let product = sumNums(6, 9);
+console.log(product);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return x * y;
 }
+
+let call = multiplyNums(70, 49);
+console.log(call);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
